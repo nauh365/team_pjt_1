4ea9eba416ac152b058e542c0520a886a1b93a1b
min2023237 <min2023237@gmail.com>
2024-03-20 14:50:05 +0900

    BE/Feat: Add comment view feature

--- sixsenses/src/main/java/com/likelion/sixsenses/service/QuestionService.java
+++ sixsenses/src/main/java/com/likelion/sixsenses/service/QuestionService.java
@@ -4,6 +4,7 @@ import com.likelion.sixsenses.entity.Question;
 import com.likelion.sixsenses.repository.QuestionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
@@ -21,6 +22,7 @@ public class QuestionService {
         return questionRepository.findAll();
     }
 
+    @Transactional
     public Optional<Question> findQuestionById(Long id) {
         return questionRepository.findById(id);
     }
